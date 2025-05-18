c1c6b79d98b7c6b6afe8fb0c04ce6fc33c8fcc4e
Darya <shelldary@yandex.ru>
2025-05-18 14:02:09 +0300

    update ticket sprint

--- TechnoparkProj/Controllers/TicketController.cs
+++ TechnoparkProj/Controllers/TicketController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using TechnoparkProj.Contracts;
 using TechnoparkProj.Core.Models;
 using TechnoparkProj.DataAccess;
 
@@ -21,6 +22,26 @@ namespace TechnoparkProj.Controllers
         {
             return await _context.Tickets
                 .ToListAsync();
+        }
+
+        [HttpPut("update-sprint")]
+        public async Task<ActionResult<IEnumerable<Ticket>>> UpdateSprintId([FromQuery] int ticketId, [FromBody] int? sprintId)
+        {
+            var ticket = await _context.Tickets
+                .Include(t => t.Sprint)
+                .FirstOrDefaultAsync(t => t.Id == ticketId);
+
+            //var sprint = await _context.Sprints.FindAsync(sprintId);
+            var sprint = await _context.Sprints.FindAsync(sprintId);
+            if (sprint == null)
+            {
+                return BadRequest($"Sprint with ID {sprintId} not found");
+            }
+            ticket.Sprint = sprint;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(ticket);
         }
     }
 }
